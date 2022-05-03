efcef359fea624fdd8766b95d9c1ecfbe8ea1cf6
Spider-Man <abeernaeem6@gmail.com>
2022-05-03 18:25:08 -0500

    Finished setting up Models and seeding

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,19 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    } ,
   },
   {
     sequelize,
@@ -18,3 +30,4 @@ ProductTag.init(
 );
 
 module.exports = ProductTag;
+
